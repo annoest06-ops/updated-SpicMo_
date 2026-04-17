daa4e2e5fe8f794722f98b45a4f27a12e5037bf5
annoest06-ops <annoest06@gmail.com>
2026-04-17 11:41:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -798,5 +798,7 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-if __name__=='__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # 10000 is safer default
+    app.run(host="0.0.0.0", port=port, debug=False)
